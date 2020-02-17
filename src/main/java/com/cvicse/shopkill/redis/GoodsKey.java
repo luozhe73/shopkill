19345b048e7056888aadcd09ad54d6f8518b83a3
luozhe73 <1043269439@qq.com>
2020-02-17 20:05:05 +0800

    页面缓存

--- src/main/java/com/cvicse/shopkill/redis/GoodsKey.java
+++ src/main/java/com/cvicse/shopkill/redis/GoodsKey.java
@@ -2,11 +2,11 @@ package com.cvicse.shopkill.redis;
 
 public class GoodsKey  extends BasePrefix{
 
-    public GoodsKey(String prefix) {
-        super(prefix);
+    private GoodsKey(int expireSeconds, String prefix) {
+        super(expireSeconds, prefix);
     }
 
-    public static GoodsKey getGoodsList = new GoodsKey("gl");
+    public static GoodsKey getGoodsList = new GoodsKey(60, "gl");
 
-    public static GoodsKey getGoodsDetail = new GoodsKey("gd");
+    public static GoodsKey getGoodsDetail = new GoodsKey(60, "gd");
 }
